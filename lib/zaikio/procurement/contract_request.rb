546474eddf646d76ba02ab9e48edb3dbff4e8e0f
Sascha Weidlich <sascha.weidlich@mac.com>
2021-01-26 11:56:04 +0100

    Cleanup contract request and supplier

--- lib/zaikio/procurement/contract_request.rb
+++ lib/zaikio/procurement/contract_request.rb
@@ -2,10 +2,11 @@ module Zaikio
   module Procurement
     class ContractRequest < Base
       uri "contract_requests(/:id)"
+      include_root_in_json :contract_request
 
       # Associations
-      has_one :supplier,  class_name: "Zaikio::Procurement::Supplier",
-                          uri: nil
+      belongs_to :supplier, class_name: "Zaikio::Procurement::Supplier",
+                            uri: nil
     end
   end
 end
--- lib/zaikio/procurement/supplier.rb
+++ lib/zaikio/procurement/supplier.rb
@@ -2,6 +2,10 @@ module Zaikio
   module Procurement
     class Supplier < Base
       uri "suppliers(/:id)"
+
+      # Associations
+      has_many :contract_requests, class_name: "Zaikio::Procurement::ContractRequest",
+                                   uri: "suppliers/:supplier_id/contract_requests(/:id)"
     end
   end
 end
