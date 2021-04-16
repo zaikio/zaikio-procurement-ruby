6915c2e06bbdaf59840483fd78c4269f448f5b8f
Sascha Weidlich <sascha.weidlich@mac.com>
2021-04-16 10:37:12 +0200

    Manually build variant association for sku to work for consumers and suppliers

--- lib/zaikio/procurement/sku.rb
+++ lib/zaikio/procurement/sku.rb
@@ -10,10 +10,14 @@ module Zaikio
                  :order_number, :unit, :weight_unit, :width, :variant_id, :created_at, :updated_at
 
       # Associations
-      has_one :variant, class_name: "Zaikio::Procurement::Variant",
-                        uri: nil
       has_many :prices, class_name: "Zaikio::Procurement::Price",
                         uri: "skus/:sku_id/prices"
+
+      # Manually build variant association to work for consumers and suppliers
+      def variant
+        path = Zaikio::Procurement.configuration.flavor == :supplier ? "substrate/variants/#{variant_id}" : "variants/#{variant_id}"
+        Zaikio::Procurement::Variant.new(self.class.request(:get, path).data)
+      end
     end
   end
 end
